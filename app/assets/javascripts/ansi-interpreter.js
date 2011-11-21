c6e6efa4b67b80b270b9bb5e815e724d679106b7
Marcin Kulik <marcin.kulik@gmail.com>
2011-11-21 23:17:11 +0100

    JS & CSS from old Sinatra project

---
+++ app/assets/javascripts/ansi-interpreter.js
@@ -0,0 +1,183 @@
+SP.AnsiInterpreter = function(terminal) {
+  this.terminal = terminal;
+  this.compilePatterns();
+}
+
+SP.AnsiInterpreter.prototype = {
+  PATTERNS: {
+    "\x07": function(data) {
+      // bell
+    },
+
+    "\x08": function(data) {
+      this.terminal.bs();
+    },
+
+    "\x0a": function(data) {
+      this.terminal.cursorDown();
+    },
+
+    "\x0d": function(data) {
+      this.terminal.cr();
+    },
+
+    "\x0e": function(data) {
+    },
+
+    "\x0f": function(data) {
+    },
+
+    "\x82": function(data) { // Reserved (?)
+    },
+
+    "\x94": function(data) { // Cancel Character, ignore previous character
+    },
+
+    // 20 - 7e
+    "([\x20-\x7e]|\xe2..|[\xc5\xc4].)+": function(data, match) {
+      this.terminal.print(match[0]);
+    },
+
+    "\x1b\\(B": function(data) { // SCS (Set G0 Character SET)
+    },
+
+    "\x1b\\[(?:[0-9]+)?(?:;[0-9]+)*([\x40-\x7e])": function(data, match) {
+      this.params = [];
+      var re = /(\d+)/g;
+      var m;
+
+      while (m = re.exec(match[0])) {
+        this.params.push(parseInt(m[1]));
+      }
+
+      this.n = this.params[0];
+      this.m = this.params[1];
+
+      this.handleCSI(match[1]);
+    },
+
+    "\x1b\\[\\?([\x30-\x3f]+)([hlsr])": function(data, match) { // private standards
+      // h = Sets DEC/xterm specific mode (http://ttssh2.sourceforge.jp/manual/en/about/ctrlseq.html#decmode)
+      // l = Resets mode (http://ttssh2.sourceforge.jp/manual/en/about/ctrlseq.html#mode)
+      // 1001 + s = ?
+      // 1001 + r = ?
+
+      if (match[1] == '1049') {
+        if (match[2] == 'h') {
+          // 1049 + h = Save cursor position, switch to alternate screen buffer, and clear screen.
+          this.terminal.saveCursor();
+          this.terminal.switchToAlternateBuffer();
+          this.terminal.clearScreen();
+        } else if (match[2] == 'l') {
+          // 1049 + l = Clear screen, switch to normal screen buffer, and restore cursor position.
+          this.terminal.clearScreen();
+          this.terminal.switchToNormalBuffer();
+          this.terminal.restoreCursor();
+        }
+      } else if (match[1] == '1002') {
+        // 2002 + h / l = mouse tracking stuff
+      } else if (match[1] == '1') {
+        // 1 + h / l = cursor keys stuff
+      }
+    },
+
+    "\x1b\x3d": function(data) { // DECKPAM - Set keypad to applications mode (ESCape instead of digits)
+    },
+
+    "\x1b\x3e": function(data) { // DECKPNM - Set keypad to numeric mode (digits intead of ESCape seq)
+    },
+
+    "\x1b\\\x5d[012]\x3b(?:.)*?\x07": function(data, match) { // OSC - Operating System Command (terminal title)
+    },
+
+    "\x1b\\[>c": function(data) { // Secondary Device Attribute request (?)
+    },
+
+    "\x1bP([^\\\\])*?\\\\": function(data) { // DCS, Device Control String
+    },
+
+    "\x1b\x37": function(data) { // save cursor pos and char attrs
+      this.terminal.saveCursor();
+    },
+
+    "\x1b\x38": function(data) { // restore cursor pos and char attrs
+      this.terminal.restoreCursor();
+    }
+  },
+
+  handleCSI: function(term) {
+    switch(term) {
+      case "@":
+        this.terminal.reserveCharacters(this.n);
+        break;
+      case "A":
+        this.terminal.cursorUp(this.n || 1);
+        break;
+      case "B":
+        this.terminal.cursorDown(this.n || 1);
+        break;
+      case "C":
+        this.terminal.cursorForward(this.n || 1);
+        break;
+      case "D":
+        this.terminal.cursorBack(this.n || 1);
+        break;
+      case "H":
+        this.terminal.setCursorPos(this.n || 1, this.m || 1);
+        break;
+      case "J":
+        this.terminal.eraseData(this.n || 0);
+        break;
+      case "K":
+        this.terminal.eraseLine(this.n || 0);
+        break;
+      case "l": // l, Reset mode
+        console.log("(TODO) reset: " + this.n);
+        break;
+      case "m":
+        this.terminal.setSGR(this.params);
+        break;
+      case "r": // Set top and bottom margins (scroll region on VT100)
+        break;
+      default:
+        console.log('no handler for CSI term: ' + term);
+    }
+  },
+
+  compilePatterns: function() {
+    this.COMPILED_PATTERNS = [];
+    var regexp;
+
+    for (re in this.PATTERNS) {
+      regexp = new RegExp('^' + re);
+      this.COMPILED_PATTERNS.push([regexp, this.PATTERNS[re]]);
+    }
+  },
+
+  feed: function(data) {
+    var match;
+    var handler;
+
+    while (data.length > 0) {
+      match = handler = null;
+
+      for (var i=0; i<this.COMPILED_PATTERNS.length; i++) {
+        var pattern = this.COMPILED_PATTERNS[i];
+        match = pattern[0].exec(data);
+        if (match) {
+          handler = pattern[1];
+          break;
+        }
+      }
+
+      if (handler) {
+        handler.call(this, data, match);
+        data = data.slice(match[0].length)
+      } else {
+        return data;
+      }
+    }
+
+    return '';
+  }
+}
---
+++ app/assets/javascripts/terminal.js
@@ -0,0 +1,305 @@
+SP.Terminal = function(cols, lines) {
+  this.cols = cols;
+  this.lines = lines;
+  this.cursorLine = 0;
+  this.cursorCol = 0;
+  this.normalBuffer = [];
+  this.alternateBuffer = [];
+  this.lineData = this.normalBuffer;
+  this.fg = this.bg = undefined;
+  this.dirtyLines = [];
+  this.initialize();
+};
+
+SP.Terminal.prototype = {
+  initialize: function() {
+    var container = $(".player .term");
+    this.element = container;
+    this.renderLine(0); // we only need 1 line
+    this.element.css({ width: this.element.width(), height: this.element.height() * this.lines });
+  },
+
+  getLine: function(n) {
+    n = (typeof n != "undefined" ? n : this.cursorLine);
+
+    var line = this.lineData[n];
+
+    if (typeof line == 'undefined') {
+      line = this.lineData[n] = [];
+      this.fill(n, 0, this.cols, ' ');
+    }
+
+    return line;
+  },
+
+  clearScreen: function() {
+    this.lineData.length = 0;
+    this.cursorLine = this.cursorCol = 0;
+    this.element.empty();
+  },
+
+  switchToNormalBuffer: function() {
+    this.lineData = this.normalBuffer;
+    this.updateScreen();
+  },
+
+  switchToAlternateBuffer: function() {
+    this.lineData = this.alternateBuffer;
+    this.updateScreen();
+  },
+
+  renderLine: function(n) {
+    var html = this.getLine(n);
+
+    if (n == this.cursorLine) {
+      html = html.slice(0, this.cursorCol).concat(['<span class="cursor">' + (html[this.cursorCol] || '') + "</span>"], html.slice(this.cursorCol + 1) || []);
+    }
+
+    var missingLines = this.lineData.length - this.element.find('.line').length;
+
+    for (var i = 0; i < missingLines; i++) {
+      var row = $('<span class="line">');
+      this.element.append(row);
+      this.element.append("\n");
+      this.element.scrollTop(100000);//row.offset().top);
+    }
+
+    this.element.find(".line:eq(" + n + ")").html(html.join(''));
+  },
+
+  renderDirtyLines: function() {
+    var updated = [];
+
+    for (var i=0; i<this.dirtyLines.length; i++) {
+      var n = this.dirtyLines[i];
+      if (updated.indexOf(n) == -1) {
+        this.renderLine(n);
+        updated.push(n);
+      }
+    }
+
+    this.dirtyLines = [];
+  },
+
+  updateLine: function(n) {
+    n = (typeof n != "undefined" ? n : this.cursorLine);
+    this.dirtyLines.push(n);
+  },
+
+  updateScreen: function() {
+    this.dirtyLines = [];
+
+    for (var l=0; l<this.lineData.length; l++) {
+      this.dirtyLines.push(l);
+    }
+  },
+
+  setSGR: function(codes) {
+    if (codes.length == 0) {
+      codes = [0];
+    }
+
+    for (var i=0; i<codes.length; i++) {
+      var n = codes[i];
+
+      if (n === 0) {
+        this.fg = this.bg = undefined;
+        this.bright = false;
+      } else if (n == 1) {
+        this.bright = true;
+      } else if (n >= 30 && n <= 37) {
+        this.fg = n - 30;
+      } else if (n >= 40 && n <= 47) {
+        this.bg = n - 40;
+      } else if (n == 38) {
+        this.fg = codes[i+2];
+        i += 2;
+      } else if (n == 48) {
+        this.bg = codes[i+2];
+        i += 2;
+      }
+    }
+  },
+
+  setCursorPos: function(line, col) {
+    line -= 1;
+    col -= 1;
+    var oldLine = this.cursorLine;
+    this.cursorLine = line;
+    this.cursorCol = col;
+    this.updateLine(oldLine);
+    this.updateLine();
+  },
+
+  saveCursor: function() {
+    this.savedCol = this.cursorCol;
+    this.savedLine = this.cursorLine;
+  },
+
+  restoreCursor: function() {
+    var oldLine = this.cursorLine;
+
+    this.cursorLine = this.savedLine;
+    this.cursorCol = this.savedCol;
+
+    this.updateLine(oldLine);
+    this.updateLine();
+  },
+
+  cursorLeft: function() {
+    if (this.cursorCol > 0) {
+      this.cursorCol -= 1;
+      this.updateLine();
+    }
+  },
+
+  cursorRight: function() {
+    if (this.cursorCol < this.cols) {
+      this.cursorCol += 1;
+      this.updateLine();
+    }
+  },
+
+  cursorUp: function() {
+    if (this.cursorLine > 0) {
+      this.cursorLine -= 1;
+      this.updateLine(this.cursorLine);
+      this.updateLine(this.cursorLine+1);
+    }
+  },
+
+  cursorDown: function() {
+    this.cursorLine += 1;
+    this.updateLine(this.cursorLine);
+    this.updateLine(this.cursorLine-1);
+  },
+
+  cursorForward: function(n) {
+    for (var i=0; i<n; i++) this.cursorRight();
+  },
+
+  cursorBack: function(n) {
+    for (var i=0; i<n; i++) this.cursorLeft();
+  },
+
+  cr: function() {
+    this.cursorCol = 0;
+    this.updateLine();
+  },
+
+  bs: function() {
+    if (this.cursorCol > 0) {
+      this.getLine()[this.cursorCol - 1] = ' ';
+      this.cursorCol -= 1;
+      this.updateLine();
+    }
+  },
+
+  print: function(text) {
+    text = Utf8.decode(text);
+
+    for (var i=0; i<text.length; i++) {
+      if (this.cursorCol >= this.cols) {
+        this.cursorLine += 1;
+        this.cursorCol = 0;
+      }
+
+      this.fill(this.cursorLine, this.cursorCol, 1, text[i]);
+      this.cursorCol += 1;
+    }
+
+    this.updateLine();
+  },
+
+  eraseData: function(n) {
+    if (n == 0) {
+      this.eraseLine(0);
+      for (var l=this.cursorLine+1; l<this.lineData.length; l++) {
+        this.lineData[l] = [];
+        this.updateLine(l);
+      }
+    } else if (n == 1) {
+      for (var l=0; l<this.cursorLine; l++) {
+        this.lineData[l] = [];
+        this.updateLine(l);
+      }
+      this.eraseLine(n);
+    } else if (n == 2) {
+      for (var l=0; l<this.lineData.length; l++) {
+        this.lineData[l] = [];
+        this.updateLine(l);
+      }
+    }
+  },
+
+  eraseLine: function(n) {
+    if (n == 0) {
+      this.fill(this.cursorLine, this.cursorCol, this.cols - this.cursorCol, ' ');
+      // this.lineData[this.cursorLine] = this.lineData[this.cursorLine].slice(0, this.cursorCol);
+      // this.lineData[this.cursorLine] = this.lineData[this.cursorLine].slice(0, this.cursorCol) + " ".times(this.cols - this.cursorCol);
+      this.updateLine();
+    } else if (n == 1) {
+      this.fill(this.cursorLine, 0, this.cursorCol, ' ');
+      // this.lineData[this.cursorLine] = " ".times(this.cursorCol).split('').concat(this.lineData[this.cursorLine].slice(this.cursorCol));
+      // this.lineData[this.cursorLine] = " ".times(this.cursorCol) + this.lineData[this.cursorLine].slice(this.cursorCol);
+      this.updateLine();
+    } else if (n == 2) {
+      this.fill(this.cursorLine, 0, this.cols, ' ');
+      // this.lineData[this.cursorLine] = [] // " ".times(this.cols);
+      this.updateLine();
+    }
+  },
+
+  reserveCharacters: function(n) {
+    var line = this.getLine();
+    this.lineData[this.cursorLine] = line.slice(0, this.cursorCol).concat(" ".times(n).split(''), line.slice(this.cursorCol, this.cols - n));
+    this.updateLine();
+  },
+
+  fill: function(line, col, n, char) {
+    var prefix = '', postfix = '';
+
+    if (this.fg !== undefined || this.bg !== undefined || this.bright) {
+      prefix = '<span class="';
+      var brightOffset = this.bright ? 8 : 0;
+
+      if (this.fg !== undefined) {
+        prefix += ' fg' + (this.fg + brightOffset);
+      } else if (this.bright) {
+        prefix += ' bright';
+      }
+
+      if (this.bg !== undefined) {
+        prefix += ' bg' + this.bg;
+      }
+
+      prefix += '">';
+      postfix = '</span>';
+    }
+
+    var char = prefix + char + postfix;
+
+    var lineArr = this.getLine(line);
+
+    for (var i=0; i<n; i++) {
+      lineArr[col+i] = char;
+    }
+  },
+
+  blinkCursor: function() {
+    var cursor = this.element.find(".cursor");
+    if (cursor.hasClass("inverted")) {
+      cursor.removeClass("inverted");
+    } else {
+      cursor.addClass("inverted");
+    }
+  },
+
+  restartCursorBlink: function() {
+    if (this.cursorTimerId) {
+      clearInterval(this.cursorTimerId);
+      this.cursorTimerId = null;
+    }
+    this.cursorTimerId = setInterval(this.blinkCursor.bind(this), 500);
+  }
+};
