99a47393abcd7827fec5b9da7f425399be0d24b4
Marcin Kulik <marcin.kulik@gmail.com>
2011-11-23 23:22:14 +0100

    Validate presence of stdout/stdout_timing

--- app/models/asciicast.rb
+++ app/models/asciicast.rb
@@ -1,8 +1,9 @@
 class Asciicast < ActiveRecord::Base
-  validates :terminal_columns, :terminal_lines, :duration, :presence => true
-
   mount_uploader :stdin, BasicUploader
   mount_uploader :stdin_timing, BasicUploader
   mount_uploader :stdout, BasicUploader
   mount_uploader :stdout_timing, BasicUploader
+
+  validates :stdout, :stdout_timing, :presence => true
+  validates :terminal_columns, :terminal_lines, :duration, :presence => true
 end
