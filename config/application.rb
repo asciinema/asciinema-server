b1eb7a30b8faa962a359cfbe32cf7b8dbdfb0aa3
Marcin Kulik <marcin.kulik@gmail.com>
2011-11-23 22:29:37 +0100

    Require activerecord in application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-# require "active_record/railtie"
+require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 # require "active_resource/railtie"
