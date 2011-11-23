9fee724e9999c16c2f13ef5fa48d7891ff5f720a
Marcin Kulik <marcin.kulik@gmail.com>
2011-11-23 22:27:38 +0100

    Rescue from RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class NotFound < StandardError; end
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  rescue_from(NotFound) { render 'exceptions/not_found' }
+  rescue_from(ActiveRecord::RecordNotFound) { render 'exceptions/not_found' }
 
 end
