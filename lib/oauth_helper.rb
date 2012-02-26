54b13fa3a6c42a9f2a3b81aeef3662752e164c1e
Micha Wrobel <sparrovv@gmail.com>
2012-02-26 17:45:38 +0100

    Add OauthHelper.get_avatar_url

---
+++ lib/oauth_helper.rb
@@ -0,0 +1,11 @@
+class OauthHelper
+
+  def self.get_avatar_url(auth)
+    if auth["provider"] == "twitter"
+      auth["info"]["image"]
+    elsif auth["provider"] == "github"
+      auth["extra_info"]["avatar_url"]
+    end
+  end
+
+end
