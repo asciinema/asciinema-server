ce67e84a6046dfe4e62b0a6eb143b175030df8f4
Micha Wrobel <sparrovv@gmail.com>
2012-02-26 17:57:46 +0100

    Don't raise AR::RecordNotFound if there is no id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.first(:id => session[:user_id]) if session[:user_id]
   end
 
   def current_user=(user)
