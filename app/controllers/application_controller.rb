ece7bfa9093f279c0525774603bde4dffc7ce9a9
Marcin Kulik <marcin.kulik@gmail.com>
2012-03-02 22:33:13 +0100

    Fix ApplicationController#current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @current_user ||= User.first(:id => session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
   def current_user=(user)
