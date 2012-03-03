9263bc37d3cc59d98c24ed2002aabf918bf0be7c
Marcin Kulik <marcin.kulik@gmail.com>
2012-03-03 08:52:10 +0100

    avatar_img helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
   def github_auth_path
     "/auth/github"
   end
+
+  def default_avatar_url
+    image_path "default_avatar.png"
+  end
+
+  def avatar_img(user)
+    image_tag user.avatar_url || default_avatar_url,
+      :title => user.nickname, :alt => user.nickname, :class => "avatar"
+  end
 end
