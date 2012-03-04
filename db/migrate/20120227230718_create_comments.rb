edb4bb3b97b6a5984243724dec21ca84fc1e19d7
Marcin Kulik <marcin.kulik@gmail.com>
2012-03-04 21:15:27 +0100

    NOT NULL for comment columns

--- db/migrate/20120227230718_create_comments.rb
+++ db/migrate/20120227230718_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.text :body
-      t.integer :user_id
-      t.integer :asciicast_id
+      t.text :body, :null => false
+      t.integer :user_id, :null => false
+      t.integer :asciicast_id, :null => false
 
       t.timestamps
     end
