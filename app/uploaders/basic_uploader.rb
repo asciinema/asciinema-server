ad9542d12418401f8e15c8ce8ccbf5bea4824a1e
Marcin Kulik <marcin.kulik@gmail.com>
2012-03-04 21:54:31 +0100

    Store files in tmp in specs

--- app/uploaders/basic_uploader.rb
+++ app/uploaders/basic_uploader.rb
@@ -13,7 +13,8 @@ class BasicUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    prefix = Rails.env.test? ? '../tmp/' : ''
+    prefix + "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
