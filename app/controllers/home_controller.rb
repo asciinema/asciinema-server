4cc0cb31a6274128efb3982c1faaed0ffa516753
Marcin Kulik <marcin.kulik@gmail.com>
2012-04-06 22:36:08 +0200

    Use decorator for asciicast on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def show
     offset = (Asciicast.featured.count * rand).to_i
-    @asciicast  = Asciicast.featured.offset(offset).first || @asciicasts.first
+    asciicast = Asciicast.featured.offset(offset).first || @asciicasts.first
+    @asciicast = AsciicastDecorator.new(asciicast)
   end
 end
