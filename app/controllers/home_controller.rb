e84f1eb3498de274dc9c4a30b95a0818542018fc
Marcin Kulik <marcin.kulik@gmail.com>
2012-04-06 22:35:09 +0200

    Don't need recent casts any more on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def show
-    @asciicasts = Asciicast.order("created_at DESC").limit(5)
     offset = (Asciicast.featured.count * rand).to_i
     @asciicast  = Asciicast.featured.offset(offset).first || @asciicasts.first
   end
