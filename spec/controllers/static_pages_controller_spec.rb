8398750aabc99dd51e47f6148e5182d22d1f46ec
Marcin Kulik <marcin.kulik@gmail.com>
2012-04-09 23:50:28 +0200

    Specs for StaticPagesController

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe StaticPagesController do
 
+  describe '#show' do
+    it 'renders template named by params[:page]' do
+      get :show, :page => 'manual'
+      response.should render_template('manual')
+    end
+  end
+
 end
