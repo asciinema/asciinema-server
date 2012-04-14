8957da3bb545f3d5218327c9ab21e5c11a84048b
Marcin Kulik <marcin.kulik@gmail.com>
2012-04-15 01:21:47 +0200

    Precompile worker-unpack.js also

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ AsciiIo::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( worker-unpack.js )
 
   config.action_mailer.delivery_method = :sendmail
 
