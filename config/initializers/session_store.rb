f8e79ffe6b22bbb2fd6fe0ab1592e11ea2e64196
Marcin Kulik <marcin.kulik@gmail.com>
2012-06-07 17:25:19 +0200

    Use ruby 1.8 syntax for hashes (rubinius/jruby)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-AsciiIo::Application.config.session_store :cookie_store, key: '_t3rminal-tv_session'
+AsciiIo::Application.config.session_store :cookie_store, :key => '_ascii_io_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
