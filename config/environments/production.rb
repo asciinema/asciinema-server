3e31b44f320fbe701b14c23ea78c45ba82b12513
Marcin Kulik <marcin.kulik@gmail.com>
2012-06-09 16:21:06 +0200

    x_sendfile_header for nginx

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ AsciiIo::Application.configure do
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
