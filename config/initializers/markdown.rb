d3ae4b65b7e173a7b982b086892f2744d6d3dc93
Marcin Kulik <marcin.kulik@gmail.com>
2012-06-10 15:28:51 +0200

    Convert newlines to br in comments

--- config/initializers/markdown.rb
+++ config/initializers/markdown.rb
@@ -1,5 +1,5 @@
 MKD_RENDERER = Redcarpet::Markdown.new(
-  Redcarpet::Render::HTML.new(:filter_html => true),
+  Redcarpet::Render::HTML.new(:filter_html => true, :hard_wrap => true),
   :no_intra_emphasis => true,
   :autolink => true
 )
