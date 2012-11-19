d72b5852e0d0b6fc700903e86227327314fcc057
Marcin Kulik <marcin.kulik@gmail.com>
2012-11-19 20:43:42 +0100

    Move carrierwave storage config to initializer

--- app/uploaders/basic_uploader.rb
+++ app/uploaders/basic_uploader.rb
@@ -7,7 +7,6 @@ class BasicUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage CFG['CARRIERWAVE_STORAGE']
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,7 @@
-if CFG['CARRIERWAVE_STORAGE'] == :fog
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+  if CFG['CARRIERWAVE_STORAGE'] == :fog
+    config.storage = :fog
+
     config.fog_credentials = {
       :provider              => 'AWS',
       :aws_access_key_id     => CFG['AWS_ACCESS_KEY_ID'],
@@ -7,5 +9,12 @@ if CFG['CARRIERWAVE_STORAGE'] == :fog
       :region                => CFG['AWS_REGION']
     }
     config.fog_directory = CFG['AWS_BUCKET']
+  else
+    config.storage = :file
+  end
+
+  if Rails.env.test?
+    config.storage = :file
+    config.enable_processing = false
   end
 end
