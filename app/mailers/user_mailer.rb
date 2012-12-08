780dff5db16c2f32295fdcf713d8941a0ebf62b9
Marcin Kulik <marcin.kulik@gmail.com>
2012-12-08 20:48:22 +0100

    Decorate users in UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,9 +4,10 @@ class UserMailer < ActionMailer::Base
 
   def new_comment_email(user, comment)
     @comment = comment
-    @author = comment.user
+    @author = UserDecorator.new(comment.user)
     @asciicast = AsciicastDecorator.new(@comment.asciicast)
 
+    user = UserDecorator.new(user)
     to = "~#{user.nickname} <#{user.email}>"
     subject = %(New comment for #{@asciicast.title})
     mail :to => to, :subject => subject
