fcc39cc1cd4222ad9e3f7550ddfd6d23b7ef9939
Marcin Kulik <marcin.kulik@gmail.com>
2013-05-27 19:54:11 +0200

    Inject factory_girl create/build methods into example groups

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,6 +26,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_base_class_for_anonymous_controllers = false
 
+  config.include FactoryGirl::Syntax::Methods
   config.include AsciiIo::ControllerMacros, :type => :controller
 
   config.before(:suite) do
