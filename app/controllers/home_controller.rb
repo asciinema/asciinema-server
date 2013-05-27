a6bbdf7a3945360b7f77308fb237703f636473e4
Marcin Kulik <marcin.kulik@gmail.com>
2013-05-27 22:03:53 +0200

    Load homepage asciicast in before_filter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,25 @@
 class HomeController < ApplicationController
+  before_filter :load_asciicast
+
   def show
     @title = "Share Your Terminal With No Fuss"
 
-    if home_asciicast_id = CFG['HOME_CAST_ID']
-      asciicast = Asciicast.find(home_asciicast_id)
-    else
-      asciicast = Asciicast.order("created_at DESC").first
-    end
-
-    if asciicast
-      @asciicast = AsciicastDecorator.new(asciicast)
+    if @asciicast
+      @asciicast = AsciicastDecorator.new(@asciicast)
     end
 
     @asciicasts = AsciicastDecorator.decorate_collection(
       Asciicast.order("created_at DESC").limit(9).includes(:user)
     )
+  end
+
+  private
+
+  def load_asciicast
+    if id = CFG['HOME_CAST_ID']
+      @asciicast = Asciicast.find(id)
+    else
+      @asciicast = Asciicast.order("created_at DESC").first
+    end
   end
 end
