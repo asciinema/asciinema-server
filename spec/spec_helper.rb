f4c9122f741829a6651a801220c59280ed348be8
Marcin Kulik <marcin.kulik@gmail.com>
2013-05-27 18:28:01 +0200

    Clean db before whole suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,22 +6,26 @@ if ENV['COVERAGE']
 end
 
 ENV["RAILS_ENV"] ||= 'test'
+
 require File.expand_path("../../config/environment", __FILE__)
+
 require 'rspec/rails'
 require 'rspec/autorun'
-
-require 'capybara/rails'
 require 'capybara/rspec'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/shared/**/*.rb")].each  { |f| require f }
 
 RSpec.configure do |config|
   config.mock_with :rspec
   config.use_transactional_fixtures = false
-  config.include AsciiIo::ControllerMacros, :type => :controller
   config.infer_base_class_for_anonymous_controllers = false
+
+  config.include AsciiIo::ControllerMacros, :type => :controller
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
