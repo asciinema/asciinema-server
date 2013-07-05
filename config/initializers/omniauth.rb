31c94b02d2e38d317aadea456a84e64dc38512e6
Marcin Kulik <marcin.kulik@gmail.com>
2013-07-05 21:40:40 +0200

    Make sure OmniAuth redirects to failure action also in dev/test env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, CFG['TWITTER_CONSUMER_KEY'], CFG['TWITTER_CONSUMER_SECRET'], :client_options => { :ssl => { :ca_path => "/etc/ssl/certs" } }
   provider :github, CFG['GITHUB_CONSUMER_KEY'], CFG['GITHUB_CONSUMER_SECRET'], :client_options => { :ssl => { :ca_path => "/etc/ssl/certs" } }
 end
+
+OmniAuth.config.on_failure = Proc.new { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
