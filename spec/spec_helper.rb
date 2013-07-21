9ef766f16bc282b846dd6af0fb42d2a3323c077f
Marcin Kulik <marcin.kulik@gmail.com>
2013-07-21 15:16:25 +0200

    Don't require rspec/autorun in spec_helper
    This is not really needed and conflicts with zeus

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rspec'
 require 'capybara/poltergeist'
 require 'sidekiq/testing'
