7e1b41dfef576f668f037d9c14a1e612812ec31e
Marcin Kulik <marcin.kulik@gmail.com>
2013-08-13 19:45:36 +0200

    Code reformating in TimingParser

--- app/parsers/timing_parser.rb
+++ app/parsers/timing_parser.rb
@@ -1,10 +1,12 @@
 class TimingParser
 
   def self.parse(data)
-    data.lines.map do |line|
-      delay, size = line.split
-      [delay.to_f, size.to_i]
-    end
+    data.lines.map { |line| parse_line(line) }
+  end
+
+  def self.parse_line(line)
+    delay, size = line.split
+    [delay.to_f, size.to_i]
   end
 
 end
