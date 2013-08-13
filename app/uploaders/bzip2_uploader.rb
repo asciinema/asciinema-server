452f5869cf99d878bc1e4b9c06625ca42f0f9601
Marcin Kulik <marcin.kulik@gmail.com>
2013-08-13 20:13:09 +0200

    Fix Bzip2Uploader#decompressed_path for S3 storage

--- app/uploaders/bzip2_uploader.rb
+++ app/uploaders/bzip2_uploader.rb
@@ -2,11 +2,13 @@ class Bzip2Uploader < BaseUploader
 
   def decompressed_path
     return unless file
-    out_path = "#{path}.decompressed"
+
+    cache_stored_file! unless cached?
+
+    out_path = "#{current_path}.decompressed"
 
     unless File.exist?(out_path)
-      cache_stored_file! unless cached?
-      system("bzip2 -d -k -c #{path} >#{out_path}")
+      system("bzip2 -d -k -c #{current_path} >#{out_path}")
     end
 
     out_path
