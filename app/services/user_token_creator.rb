f3ecd376d7228ff11476a2663ca007c242724fa9
Marcin Kulik <m@ku1ik.com>
2013-09-25 18:22:44 +0200

    Update updated_at on all claimed asciicasts

--- app/services/user_token_creator.rb
+++ app/services/user_token_creator.rb
@@ -1,18 +1,25 @@
 class UserTokenCreator
 
+  def initialize(clock = DateTime)
+    @clock = clock
+  end
+
   def create(user, token)
     user_token = user.add_user_token(token)
 
     if user_token.persisted?
-      assign_user_to_asciicasts(user_token.token, user)
+      update_asciicasts(user_token.token, user)
     end
   end
 
   private
 
-  def assign_user_to_asciicasts(token, user)
+  attr_reader :clock
+
+  def update_asciicasts(token, user)
     Asciicast.where(:user_id => nil, :user_token => token).
-    update_all(:user_id => user.id, :user_token => nil)
+    update_all(:user_id => user.id, :user_token => nil,
+               :updated_at => clock.now)
   end
 
 end
--- spec/services/user_token_creator_spec.rb
+++ spec/services/user_token_creator_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe UserTokenCreator do
 
-  let(:user_token_creator) { described_class.new }
+  let(:user_token_creator) { described_class.new(clock) }
+  let(:clock) { double('clock', now: now) }
+  let(:now) { Time.now }
 
   describe '#create' do
     let(:user) { create(:user) }
@@ -18,16 +20,21 @@ describe UserTokenCreator do
 
     context "when token was persisted" do
       let(:persisted) { true }
+      let(:old_updated_at) { 3.days.ago }
+
       let!(:asciicast_1) { create(:asciicast, :user => nil,
-                                              :user_token => token) }
+                                              :user_token => token,
+                                              :updated_at => old_updated_at) }
       let!(:asciicast_2) { create(:asciicast, :user => nil,
-                                              :user_token => 'please') }
+                                              :user_token => 'please',
+                                              :updated_at => old_updated_at) }
       let!(:asciicast_3) { create(:asciicast, :user => create(:user),
-                                              :user_token => 'nonono') }
+                                              :user_token => 'nonono',
+                                              :updated_at => old_updated_at) }
 
       it { should be(1) }
 
-      it 'assigns the user to all asciicasts with given token' do
+      it 'assigns the user to all matching asciicasts' do
         subject
 
         asciicast_1.reload; asciicast_2.reload; asciicast_3.reload
@@ -37,7 +44,7 @@ describe UserTokenCreator do
         expect(asciicast_3.user).not_to eq(user)
       end
 
-      it 'resets the token on all asciicasts with given token' do
+      it 'resets the token on all matching asciicasts' do
         subject
 
         asciicast_1.reload; asciicast_2.reload; asciicast_3.reload
@@ -45,6 +52,16 @@ describe UserTokenCreator do
         expect(asciicast_1.user_token).to be(nil)
         expect(asciicast_2.user_token).not_to be(nil)
         expect(asciicast_3.user_token).not_to be(nil)
+      end
+
+      it 'updates the updated_at field on all matching asciicasts' do
+        subject
+
+        asciicast_1.reload; asciicast_2.reload; asciicast_3.reload
+
+        expect(asciicast_1.updated_at.to_i).to eq(now.to_i)
+        expect(asciicast_2.updated_at.to_i).to eq(old_updated_at.to_i)
+        expect(asciicast_3.updated_at.to_i).to eq(old_updated_at.to_i)
       end
     end
 
