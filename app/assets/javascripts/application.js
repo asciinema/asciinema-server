bc2b91f5a2f98ad7356f96e7fc869ad4a56aa5eb
Marcin Kulik <m@ku1ik.com>
2013-10-20 17:57:50 +0200

    Extract base js assets bundle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,12 +4,8 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require jquery
+//= require base
 //= require jquery_ujs
-//= require es5-shim.min
-//= require console-shim-min
-//= require underscore-min
-//= require backbone-min
 //= require jquery.timeago
 //= require persona
 
---
+++ app/assets/javascripts/base.js
@@ -0,0 +1,5 @@
+//= require jquery
+//= require es5-shim.min
+//= require console-shim-min
+//= require underscore-min
+//= require backbone-min
