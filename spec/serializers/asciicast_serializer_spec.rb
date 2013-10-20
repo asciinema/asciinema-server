5b177f0d135e098e84e751e120098a30accf170b
Marcin Kulik <m@ku1ik.com>
2013-10-20 17:03:20 +0200

    Fix AsciicastSerializer spec

--- spec/serializers/asciicast_serializer_spec.rb
+++ spec/serializers/asciicast_serializer_spec.rb
@@ -1,15 +1,36 @@
 require 'spec_helper'
 
 describe AsciicastSerializer do
+
   let(:serializer) { described_class.new(asciicast) }
   let(:asciicast) { create(:asciicast) }
 
   describe '#to_json' do
     subject { JSON.parse(serializer.to_json) }
 
-    it { should eq({ "id" => 1, "duration" => 11.146430015563965,
-                     "stdout_frames_url" => nil, "snapshot" => nil,
-                     "width" => 96, "height" => 26 }) }
+    it 'includes id' do
+      expect(subject['id']).to eq(asciicast.id)
+    end
+
+    it 'includes duration' do
+      expect(subject['duration']).to eq(asciicast.duration)
+    end
+
+    it 'includes stdout_frames_url' do
+      expect(subject['stdout_frames_url']).to eq(asciicast.stdout_frames_url)
+    end
+
+    it 'includes snapshot' do
+      expect(subject['snapshot']).to eq(asciicast.snapshot)
+    end
+
+    it 'includes width' do
+      expect(subject['width']).to eq(asciicast.terminal_columns)
+    end
+
+    it 'includes height' do
+      expect(subject['height']).to eq(asciicast.terminal_lines)
+    end
   end
 
 end
