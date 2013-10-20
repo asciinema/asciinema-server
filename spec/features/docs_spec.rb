7b30f7045d53494ba808855df10254f8eb7ad17c
Marcin Kulik <m@ku1ik.com>
2013-10-20 17:03:12 +0200

    Fix docs specs

--- spec/features/docs_spec.rb
+++ spec/features/docs_spec.rb
@@ -13,10 +13,20 @@ feature "Docs" do
   end
 
   scenario 'Visiting getting started page' do
-    visit docs_path(:record)
+    visit docs_path(:gettingstarted)
 
     within('.main') do
       expect(page).to have_content('Getting started')
+    end
+
+    expect_doc_links
+  end
+
+  scenario 'Visiting installation page' do
+    visit docs_path(:installation)
+
+    within('.main') do
+      expect(page).to have_content('Installation')
     end
 
     expect_doc_links
@@ -27,6 +37,26 @@ feature "Docs" do
 
     within('.main') do
       expect(page).to have_content('Recorder options')
+    end
+
+    expect_doc_links
+  end
+
+  scenario 'Visiting embedding page' do
+    visit docs_path(:embedding)
+
+    within('.main') do
+      expect(page).to have_content('Embedding')
+    end
+
+    expect_doc_links
+  end
+
+  scenario 'Visiting FAQ page' do
+    visit docs_path(:faq)
+
+    within('.main') do
+      expect(page).to have_content('Frequently Asked Questions')
     end
 
     expect_doc_links
