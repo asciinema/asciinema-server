4e3c9a5d28e55fc39ce5b6c9651962ce2319abb4
Marcin Kulik <m@ku1ik.com>
2013-12-09 20:09:17 +0100

    Add config setting for using local persona js file

--- config/cfg.rb
+++ config/cfg.rb
@@ -12,7 +12,8 @@ defaults = {
   AWS_SECRET_ACCESS_KEY:   nil,
   AWS_REGION:              nil,
   AWS_BUCKET:              nil,
-  SECRET_TOKEN:            '21deaa1a1228e119434aa783ecb4af21be7513ff1f5b8c1d8894241e5fc70ad395db72c8c1b0508a0ebb994ed88a8d73f6c84e44f7a4bc554a40d77f9844d2f4'
+  SECRET_TOKEN:            '21deaa1a1228e119434aa783ecb4af21be7513ff1f5b8c1d8894241e5fc70ad395db72c8c1b0508a0ebb994ed88a8d73f6c84e44f7a4bc554a40d77f9844d2f4',
+  LOCAL_PERSONA_JS:        true
 }.stringify_keys!
 
 cfg_file = File.expand_path(File.dirname(__FILE__) + '/asciinema.yml')
@@ -37,6 +38,10 @@ module Asciinema
     def respond_to_missing?(name, include_private = false)
       key = normalize_key(name)
       @cfg.key?(key) || super
+    end
+
+    def local_persona_js?
+      local_persona_js.to_s == 'true'
     end
 
     def [](key)
