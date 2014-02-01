3fc1c0ec211a207a805297f57e2348e5f3953fab
Marcin Kulik <m@ku1ik.com>
2014-02-01 11:18:10 +0100

    Remove redundant as_json from ApplicationDecorator

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -2,10 +2,6 @@ class ApplicationDecorator < Draper::Decorator
 
   delegate_all
 
-  def as_json(*args)
-    model.as_json(*args)
-  end
-
   def markdown(text)
     MKD_SAFE_RENDERER.render(text).html_safe
   end
