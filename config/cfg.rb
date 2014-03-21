5c79fe588035dfe0f1fa16930680af78ecadd139
Marcin Kulik <m@ku1ik.com>
2014-03-21 16:43:32 +0100

    Use Virtus coercion love in config object

--- config/cfg.rb
+++ config/cfg.rb
@@ -1,59 +1,31 @@
-defaults = {
-  CARRIERWAVE_STORAGE:            'file',
-  CARRIERWAVE_STORAGE_DIR_PREFIX: 'uploads/',
-  HOME_CAST_ID:                   nil,
-  AIRBRAKE_API_KEY:               nil,
-  AIRBRAKE_HOST:                  nil,
-  GOOGLE_ANALYTICS_ID:            nil,
-  TWITTER_CONSUMER_KEY:           nil,
-  TWITTER_CONSUMER_SECRET:        nil,
-  GITHUB_CONSUMER_KEY:            nil,
-  GITHUB_CONSUMER_SECRET:         nil,
-  AWS_ACCESS_KEY_ID:              nil,
-  AWS_SECRET_ACCESS_KEY:          nil,
-  AWS_REGION:                     nil,
-  AWS_BUCKET:                     nil,
-  SECRET_TOKEN:                   '21deaa1a1228e119434aa783ecb4af21be7513ff1f5b8c1d8894241e5fc70ad395db72c8c1b0508a0ebb994ed88a8d73f6c84e44f7a4bc554a40d77f9844d2f4',
-  LOCAL_PERSONA_JS:               true,
-  SCHEME:                         'http',
-  ADD_THIS_PROFILE_ID:            nil
-}.stringify_keys!
-
-cfg_file = File.expand_path(File.dirname(__FILE__) + '/asciinema.yml')
-cfg_hash = YAML.load_file(cfg_file) || {} rescue {}
-
-cfg = {}
-cfg.merge!(defaults)
-cfg.merge!(cfg_hash)
-cfg.merge!(ENV)
-
 module Asciinema
   class Configuration
-    def initialize(cfg)
-      @cfg = cfg
-    end
+    include Virtus.model
 
-    def method_missing(name, *args, &block)
-      key = normalize_key(name)
-      @cfg.key?(key) ? @cfg[key] : super
-    end
+    attribute :add_this_profile_id,            String
+    attribute :airbrake_api_key,               String
+    attribute :airbrake_host,                  String
+    attribute :aws_access_key_id,              String
+    attribute :aws_bucket,                     String
+    attribute :aws_region,                     String
+    attribute :aws_secret_access_key,          String
+    attribute :carrierwave_storage,            String, default: 'file'
+    attribute :carrierwave_storage_dir_prefix, String, default: 'uploads/'
+    attribute :github_consumer_key,            String
+    attribute :github_consumer_secret,         String
+    attribute :google_analytics_id,            String
+    attribute :home_asciicast_id,              Integer
+    attribute :local_persona_js,               Boolean, default: false
+    attribute :scheme,                         String, default: 'http'
+    attribute :secret_token,                   String, default: '21deaa1a1228e119434aa783ecb4af21be7513ff1f5b8c1d8894241e5fc70ad395db72c8c1b0508a0ebb994ed88a8d73f6c84e44f7a4bc554a40d77f9844d2f4'
+    attribute :twitter_consumer_key,           String
+    attribute :twitter_consumer_secret,        String
 
-    def respond_to_missing?(name, include_private = false)
-      key = normalize_key(name)
-      @cfg.key?(key) || super
-    end
-
-    def [](key)
-      send(key)
-    end
-
-    def local_persona_js?
-      local_persona_js.to_s == 'true'
-    end
+    alias_method :local_persona_js?, :local_persona_js
 
     def home_asciicast
-      asciicast = if home_cast_id
-        Asciicast.find(home_cast_id)
+      asciicast = if home_asciicast_id
+        Asciicast.find(home_asciicast_id)
       else
         Asciicast.last
       end
@@ -63,12 +35,12 @@ module Asciinema
       scheme == 'https'
     end
 
-    private
-
-    def normalize_key(key)
-      key.to_s.upcase
-    end
   end
 end
+
+cfg_file = File.expand_path(File.dirname(__FILE__) + '/asciinema.yml')
+cfg = YAML.load_file(cfg_file) || {} rescue {}
+env = Hash[ENV.to_h.map { |k, v| [k.downcase, v] }]
+cfg.merge!(env)
 
 ::CFG = Asciinema::Configuration.new(cfg)
