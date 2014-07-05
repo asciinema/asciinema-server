402529c60e883fcb8f481a53b30407b1bcaef77c
Marcin Kulik <m@ku1ik.com>
2014-07-05 20:16:57 +0200

    Fix 404 handling on docs pages

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,7 +1,7 @@
 class DocsController < ApplicationController
   layout 'docs'
 
-  rescue_from ActionView::MissingTemplate, :with => :not_found
+  rescue_from ActionView::MissingTemplate, with: :handle_not_found
 
   def show
     @current_category = params[:page].to_sym
