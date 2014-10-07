2f05b3875c6426b607867a4d08c4bd1470c3ba49
Marcin Kulik <m@ku1ik.com>
2014-10-07 20:11:45 +0200

    Run Sidekiq jobs in-process in development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  require 'sidekiq/testing'
+  Sidekiq::Testing.inline!
 end
