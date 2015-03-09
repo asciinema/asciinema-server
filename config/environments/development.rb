e3dcb72118944acfde15ec7fff4dc1482cad8989
Marcin Kulik <m@ku1ik.com>
2015-03-09 11:40:24 +0100

    No shortucts, use real background job processor

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  require 'sidekiq/testing'
-  Sidekiq::Testing.inline!
 end
