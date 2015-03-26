d9f6af866f917a23d85eba3496b715087c35a7ac
Marcin Kulik <m@ku1ik.com>
2015-03-26 11:27:14 +0000

    Serve static assets in production env (in case no Nginx/Apache in front)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,8 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # Enable Rails's static asset server
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
