ae95697a9a73b755bbdfac458aa2c9862dc97147
Marcin Kulik <m@ku1ik.com>
2015-04-26 15:09:30 +0000

    Add meta tags for hiding referrer on private asciicast pages

--- app/serializers/asciicast_serializer.rb
+++ app/serializers/asciicast_serializer.rb
@@ -4,4 +4,9 @@ class AsciicastSerializer < ActiveModel::Serializer
   attributes :id, :duration, :stdout_frames_url, :snapshot
   attribute :terminal_columns, key: :width
   attribute :terminal_lines, key: :height
+
+  def private?
+    object.private?
+  end
+
 end
