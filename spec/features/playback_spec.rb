c45ee93370e79d37d9026e7bf965d95925ffaad9
Marcin Kulik <m@ku1ik.com>
2015-04-26 14:16:42 +0000

    More high level specs for private asciicasts

--- spec/features/playback_spec.rb
+++ spec/features/playback_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
-describe 'Asciicast playback', :js => true, :slow => true do
-
-  let(:asciicast) { create(:asciicast) }
+describe 'Asciicast playback', js: true, slow: true do
 
   describe "from fixture" do
     before do
@@ -14,10 +12,24 @@ describe 'Asciicast playback', :js => true, :slow => true do
       Capybara.default_wait_time = @old_wait_time
     end
 
-    it "is successful" do
-      visit asciicast_path(asciicast, speed: 5)
-      find(".start-prompt .play-button").click
-      expect(page).to have_css('.time-remaining', visible: false, text: '-00:0')
+    context "for public asciicast" do
+      let(:asciicast) { create(:asciicast, private: false) }
+
+      it "is successful" do
+        visit asciicast_path(asciicast, speed: 5)
+        find(".start-prompt .play-button").click
+        expect(page).to have_css('.time-remaining', visible: false, text: '-00:0')
+      end
+    end
+
+    context "for private asciicast" do
+      let(:asciicast) { create(:asciicast, private: true) }
+
+      it "is successful" do
+        visit asciicast_path(asciicast, speed: 5)
+        find(".start-prompt .play-button").click
+        expect(page).to have_css('.time-remaining', visible: false, text: '-00:0')
+      end
     end
   end
 
