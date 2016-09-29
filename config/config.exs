c7560d30be40230df2eab2d6704d4cf549e93d26
Marcin Kulik <m@ku1ik.com>
2016-09-29 08:10:23 +0000

    Add markdown template engine

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,9 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :phoenix, :template_engines,
+  md: PhoenixMarkdown.Engine
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
