f0b7b296c03300e7f6f1cddbeb00ca406a8096e4
Marcin Kulik <m@ku1ik.com>
2016-09-29 08:10:23 +0000

    Add markdown template engine

--- config/dev.exs
+++ config/dev.exs
@@ -22,7 +22,7 @@ config :asciinema, Asciinema.Endpoint,
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
       ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$}
+      ~r{web/templates/.*(eex|md)$}
     ]
   ]
 
