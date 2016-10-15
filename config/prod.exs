1c7bb5e16463ffd8090a0d0a84adfbf62fae4b1f
Marcin Kulik <m@ku1ik.com>
2016-10-15 08:09:26 +0000

    Configure secret_key_base and database_url via env vars

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :asciinema, Asciinema.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "asciinema.org", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -56,6 +58,8 @@ config :logger, level: :info
 #     config :asciinema, Asciinema.Endpoint, server: true
 #
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+config :asciinema, Asciinema.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "20"),
+  ssl: true
