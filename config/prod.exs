61a2a13cf800457af6841e056a7d27aadd1280af
Marcin Kulik <m@ku1ik.com>
2016-10-15 12:14:35 +0200

    Don't enforce SSL on Phoenix level

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,6 @@ use Mix.Config
 config :asciinema, Asciinema.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "asciinema.org", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
