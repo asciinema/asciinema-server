71535693431b378882d2193a6ac96c4dbbc5fdc6
Marcin Kulik <m@ku1ik.com>
2016-10-20 22:29:49 +0000

    Send static files gzipped

--- lib/asciinema/endpoint.ex
+++ lib/asciinema/endpoint.ex
@@ -8,7 +8,7 @@ defmodule Asciinema.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :asciinema, gzip: false,
+    at: "/", from: :asciinema, gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
