ffefc44f41cf0a90dbe0053d4a1e7213720edb15
Marcin Kulik <m@ku1ik.com>
2017-01-09 19:33:01 +0100

    Switch to aleph

--- project.clj
+++ project.clj
@@ -11,6 +11,7 @@
                  [ring/ring-defaults "0.2.1"]
                  [ring-jetty-component "0.3.1"]
                  [ring-webjars "0.1.1"]
+                 [aleph "0.4.1"]
                  [org.slf4j/slf4j-nop "1.7.21"]
                  [org.webjars/normalize.css "3.0.2"]
                  [duct/hikaricp-component "0.1.0"]
