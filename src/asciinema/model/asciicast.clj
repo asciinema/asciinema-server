be4995c9f93058b999c7a64876575ef2a6a034b6
Marcin Kulik <m@ku1ik.com>
2017-02-13 19:52:53 +0100

    Add asciicast model ns

---
+++ src/asciinema/model/asciicast.clj
@@ -0,0 +1,13 @@
+(ns asciinema.model.asciicast)
+
+(defn json-store-path [{:keys [id file stdout_frames]}]
+  (cond
+    file (str "asciicast/file/" id "/" file)
+    stdout_frames (str "asciicast/stdout_frames/" id "/" stdout_frames)))
+
+(def default-theme "asciinema")
+
+(defn theme-name [asciicast user]
+  (or (:theme_name asciicast) ; ensure we don't store empty strings in db
+      (:theme_name user)
+      default-theme))
