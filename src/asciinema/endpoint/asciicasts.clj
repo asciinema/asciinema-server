0eda5c040c7450054ac00be34d5b63bf315566b9
Marcin Kulik <m@ku1ik.com>
2017-02-14 12:15:51 +0100

    Endpoint for serving recording JSON file

---
+++ src/asciinema/endpoint/asciicasts.clj
@@ -0,0 +1,21 @@
+(ns asciinema.endpoint.asciicasts
+  (:require [asciinema.boundary
+             [asciicast-database :as adb]
+             [file-server :as fserver]]
+            [asciinema.model.asciicast :as asciicast]
+            [compojure.api.sweet :refer :all]
+            [ring.util.http-response :as response]
+            [schema.core :as s]))
+
+(defn asciicasts-endpoint [{:keys [db file-server]}]
+  (api
+   (context
+    "/a" []
+    (GET "/:token.json" []
+         :path-params [token :- String]
+         :query-params [{dl :- s/Bool false}]
+         (if-let [asciicast (adb/get-asciicast-by-token db token)]
+           (let [path (asciicast/json-store-path asciicast)
+                 filename (str "asciicast-" (:id asciicast) ".json")]
+             (fserver/serve file-server path (when dl {:filename filename})))
+           (response/not-found))))))
