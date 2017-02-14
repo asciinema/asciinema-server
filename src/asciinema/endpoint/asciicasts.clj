a87b76cc60b9d21cb162a0f43a446818cc796995
Marcin Kulik <m@ku1ik.com>
2017-02-14 15:33:43 +0100

    Prevent compojure-api handle unknown exceptions

--- src/asciinema/endpoint/asciicasts.clj
+++ src/asciinema/endpoint/asciicasts.clj
@@ -7,8 +7,12 @@
             [ring.util.http-response :as response]
             [schema.core :as s]))
 
+(defn exception-handler [^Exception e data request]
+  (throw e))
+
 (defn asciicasts-endpoint [{:keys [db file-server]}]
   (api
+   {:exceptions {:handlers {:compojure.api.exception/default exception-handler}}}
    (context
     "/a" []
     (GET "/:token.json" []
