6943bceccab639f5f71815b078299664f499a6d7
Marcin Kulik <m@ku1ik.com>
2017-02-15 16:53:24 +0100

    Remove example endpoint

--- src/asciinema/endpoint/example.clj
+++
@@ -1,8 +0,0 @@
-(ns asciinema.endpoint.example
-  (:require [compojure.core :refer :all]
-            [clojure.java.io :as io]))
-
-(defn example-endpoint [{{db :spec} :db}]
-  (context "/example" []
-    (GET "/" []
-      (io/resource "asciinema/endpoint/example/example.html"))))
