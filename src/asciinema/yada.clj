1e08b39e4990cf4acb215a14752b25c6a78c8a80
Marcin Kulik <m@ku1ik.com>
2017-02-28 18:32:37 +0100

    Default 404 page

--- src/asciinema/yada.clj
+++ src/asciinema/yada.clj
@@ -1,5 +1,15 @@
 (ns asciinema.yada
-  (:require [yada.yada :as yada]))
+  (:require [clojure.java.io :as io]
+            [yada.yada :as yada]))
+
+(defn not-found-response [ctx]
+  (case (yada/content-type ctx)
+    "text/html" (io/input-stream (io/resource "asciinema/errors/404.html"))
+    "Not found"))
 
 (defn resource [model]
-  (yada/resource model))
+  (-> model
+      (update-in [:responses 404] #(or %
+                                       {:produces #{"text/html" "text/plain"}
+                                        :response not-found-response}))
+      yada/resource))
