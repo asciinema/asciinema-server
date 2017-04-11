d7e2f9702fe8709b160e0daa6ecd0d0d2478d022
Marcin Kulik <m@ku1ik.com>
2017-04-11 09:23:41 +0000

    Upgrade a2png

--- app/services/rasterizer.rb
+++ app/services/rasterizer.rb
@@ -5,7 +5,7 @@ class Rasterizer
   BINARY_PATH = (Rails.root + "a2png" + "a2png.sh").to_s
 
   def generate_image(asciicast_url, out_path, time, scale, theme)
-    o, e, t = Open3.capture3("#{BINARY_PATH} -s #{scale} -t #{theme} '#{asciicast_url}' '#{out_path}' #{time}")
+    o, e, t = Open3.capture3("#{BINARY_PATH} '#{asciicast_url}' '#{out_path}' #{time} #{theme} #{scale}")
 
     if t.exitstatus != 0
       raise RuntimeError, "Couldn't generate image from #{asciicast_url}:\n#{o}\n#{e}"
