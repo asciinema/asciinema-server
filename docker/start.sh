3ca0767faa08f1e66f2a36dee9af93d51ffdc9cd
Marcin Kulik <m@ku1ik.com>
2017-04-22 19:08:11 +0200

    Don't pass -Xmx to java, use _JAVA_OPTIONS env var if needed

--- docker/start.sh
+++ docker/start.sh
@@ -5,4 +5,4 @@ export S3_ACCESS_KEY=${AWS_ACCESS_KEY_ID}
 export S3_SECRET_KEY=${AWS_SECRET_ACCESS_KEY}
 export A2PNG_BIN_PATH="/app/a2png/a2png.sh"
 
-exec java -server -Xmx3g -jar /app/target/uberjar/asciinema-0.1.0-SNAPSHOT-standalone.jar
+exec java -server -jar /app/target/uberjar/asciinema-0.1.0-SNAPSHOT-standalone.jar
