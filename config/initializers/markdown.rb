09264313d7be0cb06e23440a59a4c73d1ea8018e
Marcin Kulik <m@ku1ik.com>
2017-05-02 15:58:18 +0200

    Generate ToC links in Markdown output

--- config/initializers/markdown.rb
+++ config/initializers/markdown.rb
@@ -1,11 +1,11 @@
 MKD_SAFE_RENDERER = Redcarpet::Markdown.new(
-  Redcarpet::Render::HTML.new(:filter_html => true, :hard_wrap => true),
+  Redcarpet::Render::HTML.new(:filter_html => true, :hard_wrap => true, :with_toc_data => true),
   :no_intra_emphasis => true,
   :autolink => true
 )
 
 MKD_RENDERER = Redcarpet::Markdown.new(
-  Redcarpet::Render::HTML.new,
+  Redcarpet::Render::HTML.new(:with_toc_data => true),
   :no_intra_emphasis => true,
   :autolink => true
 )
