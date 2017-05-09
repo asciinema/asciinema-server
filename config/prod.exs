ce5f5ae778d2e16877ff2a3e2cbad0be18fbd9a1
Marcin Kulik <m@ku1ik.com>
2017-05-09 19:27:35 +0200

    Don't require ssl connection for postgres

--- config/prod.exs
+++ config/prod.exs
@@ -60,4 +60,4 @@ config :asciinema, Asciinema.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "20"),
-  ssl: true
+  ssl: false
