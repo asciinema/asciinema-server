f74b3edfed31e9fac011ad18ad7e27b1d74779d4
Marcin Kulik <m@ku1ik.com>
2017-05-16 12:31:24 +0200

    Sort deps and applications in mix.exs

--- mix.exs
+++ mix.exs
@@ -18,8 +18,18 @@ defmodule Asciinema.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Asciinema, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :plug_rails_cookie_session_store, :bugsnag]]
+     applications: [
+       :bugsnag,
+       :cowboy,
+       :gettext,
+       :logger,
+       :phoenix,
+       :phoenix_ecto,
+       :phoenix_html,
+       :phoenix_pubsub,
+       :plug_rails_cookie_session_store,
+       :postgrex,
+     ]]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,18 +40,20 @@ defmodule Asciinema.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:phoenix_markdown, "~> 0.1"},
-     {:plug_rails_cookie_session_store, "~> 0.1"},
-     {:plugsnag, "~> 1.2.0"},
-     {:poison, "~> 2.2"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:cowboy, "~> 1.0"},
+      {:gettext, "~> 0.11"},
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:phoenix_markdown, "~> 0.1"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:plug_rails_cookie_session_store, "~> 0.1"},
+      {:plugsnag, "~> 1.2.0"},
+      {:poison, "~> 2.2"},
+      {:postgrex, ">= 0.0.0"},
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
