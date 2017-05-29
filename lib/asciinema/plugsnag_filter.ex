7a07fdfc81c81bce1a90f6a1da3c765575427d14
Marcin Kulik <m@ku1ik.com>
2017-05-29 14:06:03 +0200

    Use fork of plugsnag that ignores Phoenix.NotAcceptableError

--- lib/asciinema/plugsnag_filter.ex
+++
@@ -1,9 +0,0 @@
-defmodule Asciinema.PlugsnagFilter do
-  defmacro __using__(_options \\ []) do
-    quote location: :keep do
-      def handle_errors(_conn, %{reason: %Phoenix.NotAcceptableError{}}) do
-        nil
-      end
-    end
-  end
-end
