c64bb379aaf9c54c5918fc8383d8b305cc68b265
Marcin Kulik <m@ku1ik.com>
2017-06-23 09:35:56 +0200

    Display tmp username for current user when no username nor email set

--- app/decorators/current_user_decorator.rb
+++ app/decorators/current_user_decorator.rb
@@ -1,7 +1,7 @@
 class CurrentUserDecorator < UserDecorator
 
   def display_name
-    model.username || model.email
+    model.username || model.email || model.temporary_username || "Me"
   end
 
 end
