640e0e227b28cb87bd41071f5984d887d67ac88d
Marcin Kulik <m@ku1ik.com>
2017-07-16 20:03:43 +0200

    Allow overriding config via config/custom.exs

--- config/config.exs
+++ config/config.exs
@@ -79,3 +79,6 @@ config :exq_ui,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+# Import custom config.
+import_config "custom*.exs"
