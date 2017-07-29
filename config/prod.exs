f249d4a276137d6014186bddd4633169f507f38f
Marcin Kulik <m@ku1ik.com>
2017-07-29 12:56:14 +0200

    Upgrade Phoenix to 1.3

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :asciinema, Asciinema.Endpoint,
   url: [scheme: System.get_env("URL_SCHEME") || "https",
         host: System.get_env("URL_HOST") || "asciinema.org",
         port: String.to_integer(System.get_env("URL_PORT") || "443")],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
