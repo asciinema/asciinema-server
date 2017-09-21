03614b16e4b81bc4108bc5f17661696c02c1d8d8
Marcin Kulik <m@ku1ik.com>
2017-09-21 11:26:48 +0200

    Tag test requiring compiled vt script

--- test/asciinema/asciicasts_test.exs
+++ test/asciinema/asciicasts_test.exs
@@ -144,6 +144,7 @@ defmodule Asciinema.AsciicastsTest do
   end
 
   describe "generate_snapshot/2" do
+    @tag :vt
     test "returns list of screen lines" do
       stdout_stream = [{1.0, "a"}, {0.5, "b"}, {2.0, "c"}]
       snapshot = Asciicasts.generate_snapshot(stdout_stream, 4, 2, 2.5)
