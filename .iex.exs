6af82e406309db37c886eec75882067ee34d3247
Marcin Kulik <m@ku1ik.com>
2017-11-09 20:23:09 +0100

    Import Ecto in iex if available

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,6 @@
+import_if_available Ecto
+import_if_available Ecto.Query
+
 alias Asciinema.{Repo, Asciicasts, Accounts}
 alias Asciinema.Asciicasts.Asciicast
 alias Asciinema.Accounts.{User, ApiToken}
