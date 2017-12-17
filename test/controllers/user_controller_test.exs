7e21cd3832e5e1018ac2f73ba641ee50fd28c655
Marcin Kulik <m@ku1ik.com>
2017-12-17 13:44:29 +0100

    Group sign up test with describe block

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -7,41 +7,43 @@ defmodule Asciinema.UserControllerTest do
     {:ok, conn: conn}
   end
 
-  test "successful sign-up", %{conn: conn} do
-    conn = get conn, "/users/new", t: Accounts.signup_token("test@example.com")
-    assert redirected_to(conn, 302) == "/users/new"
+  describe "sign-up" do
+    test "successful sign-up", %{conn: conn} do
+      conn = get conn, "/users/new", t: Accounts.signup_token("test@example.com")
+      assert redirected_to(conn, 302) == "/users/new"
 
-    conn = get conn, "/users/new"
-    assert html_response(conn, 200)
+      conn = get conn, "/users/new"
+      assert html_response(conn, 200)
 
-    conn = post conn, "/users"
-    assert redirected_to(conn, 302) == "/username/new"
-    assert get_rails_flash(conn, :notice) =~ ~r/welcome/i
-  end
+      conn = post conn, "/users"
+      assert redirected_to(conn, 302) == "/username/new"
+      assert get_rails_flash(conn, :notice) =~ ~r/welcome/i
+    end
 
-  test "failed sign-up due to email taken", %{conn: conn} do
-    insert(:user, email: "test@example.com")
+    test "failed sign-up due to email taken", %{conn: conn} do
+      insert(:user, email: "test@example.com")
 
-    conn = get conn, "/users/new", t: Accounts.signup_token("test@example.com")
-    assert redirected_to(conn, 302) == "/users/new"
+      conn = get conn, "/users/new", t: Accounts.signup_token("test@example.com")
+      assert redirected_to(conn, 302) == "/users/new"
 
-    conn = get conn, "/users/new"
-    assert html_response(conn, 200)
+      conn = get conn, "/users/new"
+      assert html_response(conn, 200)
 
-    conn = post conn, "/users"
-    assert redirected_to(conn, 302) == "/login/new"
-    assert get_flash(conn, :error) =~ ~r/already/i
-  end
+      conn = post conn, "/users"
+      assert redirected_to(conn, 302) == "/login/new"
+      assert get_flash(conn, :error) =~ ~r/already/i
+    end
 
-  test "failed sign-up due to invalid token", %{conn: conn} do
-    conn = get conn, "/users/new", t: "nope"
-    assert redirected_to(conn, 302) == "/users/new"
+    test "failed sign-up due to invalid token", %{conn: conn} do
+      conn = get conn, "/users/new", t: "nope"
+      assert redirected_to(conn, 302) == "/users/new"
 
-    conn = get conn, "/users/new"
-    assert html_response(conn, 200)
+      conn = get conn, "/users/new"
+      assert html_response(conn, 200)
 
-    conn = post conn, "/users"
-    assert redirected_to(conn, 302) == "/login/new"
-    assert get_flash(conn, :error) =~ ~r/invalid/i
+      conn = post conn, "/users"
+      assert redirected_to(conn, 302) == "/login/new"
+      assert get_flash(conn, :error) =~ ~r/invalid/i
+    end
   end
 end
