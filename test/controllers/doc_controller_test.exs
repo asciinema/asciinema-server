c7fe53c7c48e2b06eb52595ad7b0316b18c29cc9
Marcin Kulik <m@ku1ik.com>
2018-03-08 21:20:50 +0100

    Link to new FAQ on the forum

--- test/controllers/doc_controller_test.exs
+++ test/controllers/doc_controller_test.exs
@@ -12,11 +12,9 @@ defmodule Asciinema.DocControllerTest do
                "/docs/installation",
                "/docs/usage",
                "/docs/config",
-               "/docs/embedding",
-               "/docs/faq"], fn(path) ->
+               "/docs/embedding"], fn(path) ->
       conn = get conn, path
       assert html_response(conn, 200) =~ "<h2>Docs</h2>"
     end)
   end
-
 end
