b45cf142826c2b44ff48f6f3ed4c6c9873cce93a
Marcin Kulik <m@ku1ik.com>
2018-03-18 12:04:39 +0100

    More tests for explore page

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,10 +1,21 @@
 defmodule Asciinema.Factory do
   use ExMachina.Ecto, repo: Asciinema.Repo
   alias Asciinema.Accounts.User
+  alias Asciinema.Asciicasts.Asciicast
 
   def user_factory do
     %User{username: sequence(:username, &"username-#{&1}"),
           email: sequence(:email, &"email-#{&1}@example.com"),
           auth_token: Crypto.random_token(20)}
+  end
+
+  def asciicast_factory do
+    %Asciicast{user: build(:user),
+               version: 2,
+               duration: 123.45,
+               terminal_columns: 80,
+               terminal_lines: 24,
+               secret_token: sequence(:secret_token, &"sekrit-#{&1}"),
+               snapshot: [[["foo", %{}]], [["bar", %{}]]]}
   end
 end
