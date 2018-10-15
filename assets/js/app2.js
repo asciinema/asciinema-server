fd8e14b4b141c2731759f65636ec55ba7437f720
Marcin Kulik <m@ku1ik.com>
2018-10-15 21:04:38 +0200

    Don't send referrer header for links on private asciicast page

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -7,4 +7,8 @@ import "phoenix_html";
 $(function() {
   $('input[data-behavior=focus]:first').focus().select();
   $('[data-toggle="popover"]').popover({ html: true });
+
+  if ($('meta[name=referrer][content=origin]').length > 0) {
+    $('a[href*=http]').attr('rel', 'noreferrer');
+  }
 });
