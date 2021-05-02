79537b9e6a052e970483a9e081cd1f102fc5fdc9
Marcin Kulik <m@ku1ik.com>
2021-05-02 16:55:40 +0200

    Fix tests

--- test/asciinema/accounts_test.exs
+++ test/asciinema/accounts_test.exs
@@ -1,8 +1,59 @@
 defmodule Asciinema.AccountsTest do
   import Asciinema.Fixtures
   use Asciinema.DataCase
+  use Oban.Testing, repo: Asciinema.Repo
   alias Asciinema.Accounts
   alias Asciinema.Accounts.User
+
+  describe "send_login_email/1" do
+    defp signup_url(_), do: "http://signup"
+    defp login_url(_), do: "http://login"
+
+    test "existing user, by email" do
+      user = fixture(:user)
+
+      assert Accounts.send_login_email(user.email, &signup_url/1, &login_url/1) == :ok
+
+      assert_enqueued(
+        worker: Asciinema.Emails.Job,
+        args: %{"type" => "login", "to" => user.email, "url" => "http://login"}
+      )
+    end
+
+    test "existing user, by username" do
+      user = fixture(:user)
+
+      assert Accounts.send_login_email(user.username, &signup_url/1, &login_url/1) == :ok
+
+      assert_enqueued(
+        worker: Asciinema.Emails.Job,
+        args: %{"type" => "login", "to" => user.email, "url" => "http://login"}
+      )
+    end
+
+    test "non-existing user, by email" do
+      assert Accounts.send_login_email("new@example.com", &signup_url/1, &login_url/1) == :ok
+
+      assert_enqueued(
+        worker: Asciinema.Emails.Job,
+        args: %{"type" => "signup", "to" => "new@example.com", "url" => "http://signup"}
+      )
+    end
+
+    test "non-existing user, by email, when email is invalid" do
+      assert Accounts.send_login_email("new@", &signup_url/1, &login_url/1) ==
+               {:error, :email_invalid}
+
+      refute_enqueued(worker: Asciinema.Emails.Job)
+    end
+
+    test "non-existing user, by username" do
+      assert Accounts.send_login_email("idontexist", &signup_url/1, &login_url/1) ==
+               {:error, :user_not_found}
+
+      refute_enqueued(worker: Asciinema.Emails.Job)
+    end
+  end
 
   describe "verify_signup_token/1" do
     test "invalid token" do
