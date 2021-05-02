f50ed461f456dee9ec9840d6720c7e6a10d2c65d
Marcin Kulik <m@ku1ik.com>
2021-05-02 12:44:20 +0200

    Upgrade Phoenix to 1.5

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -18,7 +18,8 @@ defmodule AsciinemaWeb.ConnCase do
   using do
     quote do
       # Import conveniences for testing with connections
-      use Phoenix.ConnTest
+      import Plug.Conn
+      import Phoenix.ConnTest
 
       import AsciinemaWeb.Router.Helpers.Extra
       import Asciinema.Fixtures
