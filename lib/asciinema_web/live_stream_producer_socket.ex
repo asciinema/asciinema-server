89b4fd34aace03eeee3f37a7906b63d2458a6863
Marcin Kulik <m@ku1ik.com>
2023-03-08 19:01:17 +0100

    Add vt size guards for live stream producer

--- lib/asciinema_web/live_stream_producer_socket.ex
+++ lib/asciinema_web/live_stream_producer_socket.ex
@@ -33,16 +33,21 @@ defmodule AsciinemaWeb.LiveStreamProducerSocket do
     {:ok, state}
   end
 
+  @max_cols 720
+  @max_rows 200
+
   def handle_in({text, _opts}, state) do
     result =
       case Jason.decode(text) do
-        # TODO add guard for positive cols/rows
-        {:ok, %{"cols" => cols, "rows" => rows}} when is_integer(cols) and is_integer(rows) ->
+        {:ok, %{"cols" => cols, "rows" => rows}}
+        when is_integer(cols) and is_integer(rows) and cols > 0 and rows > 0 and cols <= @max_cols and
+               rows <= @max_rows ->
           Logger.info("producer/#{state.stream_id}: reset (#{cols}x#{rows})")
           LiveStream.reset(state.stream_id, {cols, rows})
 
-        # TODO add guard for positive cols/rows
-        {:ok, %{"width" => cols, "height" => rows}} when is_integer(cols) and is_integer(rows) ->
+        {:ok, %{"width" => cols, "height" => rows}}
+        when is_integer(cols) and is_integer(rows) and cols > 0 and rows > 0 and cols <= @max_cols and
+               rows <= @max_rows ->
           Logger.info("producer/#{state.stream_id}: reset (#{cols}x#{rows})")
           LiveStream.reset(state.stream_id, {cols, rows})
 
