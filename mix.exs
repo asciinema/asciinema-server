0bfb919396a9dee17e8b5cf53f7ebe485c7a20ce
Marcin Kulik <m@ku1ik.com>
2023-03-22 15:45:57 +0100

    Start ssl app explicitly

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Asciinema.MixProject do
   def application do
     [
       mod: {Asciinema.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 
