8696a9b7f560e3960877d5fc99b008e96e1ad6be
Marcin Kulik <m@ku1ik.com>
2023-07-01 13:45:12 +0200

    Improve logging in live stream sockets

--- lib/asciinema_web/live_stream_producer_socket.ex
+++ lib/asciinema_web/live_stream_producer_socket.ex
@@ -213,9 +213,8 @@ defmodule AsciinemaWeb.LiveStreamProducerSocket do
 
   @impl true
   def terminate(reason, state) do
-    Logger.info(
-      "producer/#{state.stream_id}: terminating | reason: #{inspect(reason)}, state: #{inspect(state)}"
-    )
+    Logger.info("producer/#{state.stream_id}: terminating (#{inspect(reason)})")
+    Logger.debug("producer/#{state.stream_id}: state: #{inspect(state)}")
 
     :ok
   end
