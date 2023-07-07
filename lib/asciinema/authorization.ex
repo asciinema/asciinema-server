8922c245493c0842e11fb7343766b1a9102aac6c
Marcin Kulik <m@ku1ik.com>
2023-07-07 21:38:51 +0200

    Make Authorization.Policy more concise

--- lib/asciinema/authorization.ex
+++ lib/asciinema/authorization.ex
@@ -4,32 +4,14 @@ defmodule Asciinema.Authorization do
   alias Asciinema.Streaming.LiveStream
 
   defmodule Policy do
-    def can?(nil, _action, _thing) do
-      false
-    end
-
-    def can?(%User{is_admin: true}, _action, _thing) do
-      true
-    end
-
+    def can?(nil, _action, _thing), do: false
+    def can?(%User{is_admin: true}, _action, _thing), do: true
     def can?(_user, :make_featured, %Asciicast{}), do: false
     def can?(_user, :make_not_featured, %Asciicast{}), do: false
-
-    def can?(user, _action, %Asciicast{user_id: uid}) do
-      user.id == uid
-    end
-
-    def can?(user, _action, %LiveStream{user_id: uid}) do
-      user.id == uid
-    end
-
-    def can?(user, :update, %User{id: uid}) do
-      user.id == uid
-    end
-
-    def can?(_user, _action, _thing) do
-      false
-    end
+    def can?(user, _action, %Asciicast{user_id: uid}), do: user.id == uid
+    def can?(user, _action, %LiveStream{user_id: uid}), do: user.id == uid
+    def can?(user, :update, %User{id: uid}), do: user.id == uid
+    def can?(_user, _action, _thing), do: false
   end
 
   def can?(user, action, thing) do
