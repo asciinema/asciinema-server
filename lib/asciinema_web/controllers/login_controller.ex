8bce0cb64efb1af5b3f7e72d937f21502d7c3112
Marcin Kulik <m@ku1ik.com>
2023-07-17 15:40:05 +0200

    Use new-style controller for login page

--- lib/asciinema_web/controllers/login_controller.ex
+++ lib/asciinema_web/controllers/login_controller.ex
@@ -1,10 +1,10 @@
 defmodule AsciinemaWeb.LoginController do
-  use AsciinemaWeb, :controller
+  use AsciinemaWeb, :new_controller
 
   plug :clear_main_class
 
   def new(conn, _params) do
-    render(conn, "new.html")
+    render(conn, :new)
   end
 
   def create(conn, %{"login" => %{"email" => identifier}}) do
@@ -19,20 +19,20 @@ defmodule AsciinemaWeb.LoginController do
 
     case result do
       :ok ->
-        redirect(conn, to: Routes.login_path(conn, :sent))
+        redirect(conn, to: ~p"/login/sent")
 
       {:error, :user_not_found} ->
-        render(conn, "new.html", error: "No user found for given username.")
+        render(conn, :new, error: "No user found for given username.")
 
       {:error, :email_invalid} ->
-        render(conn, "new.html", error: "This doesn't look like a correct email address.")
+        render(conn, :new, error: "This doesn't look like a correct email address.")
 
       {:error, :email_missing} ->
-        redirect(conn, to: Routes.login_path(conn, :sent))
+        redirect(conn, to: ~p"/login/sent")
     end
   end
 
   def sent(conn, _params) do
-    render(conn, "sent.html")
+    render(conn, :sent)
   end
 end
