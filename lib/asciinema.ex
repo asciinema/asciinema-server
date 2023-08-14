48ed91d6328bcd11be7502ac0a1f5ac644985770
Marcin Kulik <m@ku1ik.com>
2023-08-14 11:36:14 +0200

    Add Repo.transact/2
    Thanks https://tomkonidas.com/repo-transact/ !

--- lib/asciinema.ex
+++ lib/asciinema.ex
@@ -31,12 +31,13 @@ defmodule Asciinema do
   defdelegate verify_login_token(token), to: Accounts
 
   def merge_accounts(src_user, dst_user) do
-    Repo.transaction(fn ->
+    Repo.transact(fn ->
       Recordings.reassign_asciicasts(src_user.id, dst_user.id)
       Streaming.reassign_live_streams(src_user.id, dst_user.id)
       Accounts.reassign_api_tokens(src_user.id, dst_user.id)
       Accounts.delete_user!(src_user)
-      Accounts.get_user(dst_user.id)
+
+      {:ok, Accounts.get_user(dst_user.id)}
     end)
   end
 
