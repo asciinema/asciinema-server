73e03f3b68f6a842b2272dc03085e8133f5f216d
Marcin Kulik <m@ku1ik.com>
2023-08-18 10:45:00 +0200

    Persist current terminal size in periodic stream updates

--- lib/asciinema/streaming/live_stream_server.ex
+++ lib/asciinema/streaming/live_stream_server.ex
@@ -175,7 +175,14 @@ defmodule Asciinema.Streaming.LiveStreamServer do
 
   def handle_info(:update_stream, state) do
     Process.send_after(self(), :update_stream, @update_stream_interval)
-    stream = Streaming.update_live_stream(state.stream, current_viewer_count: state.viewer_count)
+    {cols, rows} = state.vt_size
+
+    stream =
+      Streaming.update_live_stream(state.stream,
+        current_viewer_count: state.viewer_count,
+        cols: cols,
+        rows: rows
+      )
 
     {:noreply, %{state | stream: stream}}
   end
