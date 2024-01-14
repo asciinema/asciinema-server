d580ea1e1578116ee83dca06a0c13dbc96d916fe
Marcin Kulik <m@ku1ik.com>
2024-01-14 21:48:13 +0100

    Upgrade Oban

---
+++ priv/repo/migrations/20240114204549_upgrade_oban_jobs_to_v12.exs
@@ -0,0 +1,7 @@
+defmodule Asciinema.Repo.Migrations.UpgradeObanJobsToV12 do
+  use Ecto.Migration
+
+  def up, do: Oban.Migrations.up(version: 12)
+
+  def down, do: Oban.Migrations.down(version: 11)
+end
