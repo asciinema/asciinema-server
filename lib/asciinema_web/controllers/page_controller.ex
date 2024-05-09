3a02165415884f4656db5c6c78780f58fb7c4342
Marcin Kulik <m@ku1ik.com>
2024-05-09 11:41:44 +0200

    Automatically wrap all templates of page controller in a container

--- lib/asciinema_web/controllers/page_controller.ex
+++ lib/asciinema_web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule AsciinemaWeb.PageController do
   use AsciinemaWeb, :controller
 
+  plug :wrap_in_container
+
   def about(conn, _params) do
     render(
       conn,
@@ -9,4 +11,6 @@ defmodule AsciinemaWeb.PageController do
       contact_email_address: Application.get_env(:asciinema, :contact_email_address)
     )
   end
+
+  defp wrap_in_container(conn, _), do: assign(conn, :main_class, "container")
 end
