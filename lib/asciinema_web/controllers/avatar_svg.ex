85d474f990beb4dd66744104641a6a5f8cca6bb4
Marcin Kulik <m@ku1ik.com>
2024-06-15 22:09:49 +0200

    Tweak identicon avatars

--- lib/asciinema_web/controllers/avatar_svg.ex
+++ lib/asciinema_web/controllers/avatar_svg.ex
@@ -2,6 +2,6 @@ defmodule AsciinemaWeb.AvatarSVG do
   def show(%{user: user}) do
     email = user.email || "#{user.id}@asciinema"
 
-    {:safe, IdenticonSvg.generate(email, 6, :basic)}
+    {:safe, IdenticonSvg.generate(email, 7, :split2, 1.0, 1)}
   end
 end
