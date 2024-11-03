c532f600ea210eb74eb91ef7f9a9d259d85a0606
Marcin Kulik <m@ku1ik.com>
2024-11-03 15:45:51 +0100

    Fix RGB colors rendering in SVG

--- test/controllers/recording_svg_test.exs
+++ test/controllers/recording_svg_test.exs
@@ -11,6 +11,22 @@ defmodule Asciinema.RecordingSvgTest do
 
       assert svg =~ ~r/^<\?xml.+foobar.+bazqux/s
     end
+
+    test "supports RGB color in fg/bg text attrs" do
+      asciicast =
+        build(:asciicast,
+          snapshot: [
+            [["foo", %{"fg" => [16, 32, 48]}], ["bar", %{"bg" => "rgb(64,80,96)"}]],
+            [["baz", %{"fg" => "#708090"}]]
+          ]
+        )
+
+      svg = render_svg(asciicast)
+
+      assert svg =~ "#102030"
+      assert svg =~ "rgb(64,80,96)"
+      assert svg =~ "#708090"
+    end
   end
 
   defp render_svg(asciicast) do
