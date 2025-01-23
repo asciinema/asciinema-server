47913c258f3a5a1602e4b22e4c500fea5db1c488
Marcin Kulik <m@ku1ik.com>
2025-01-23 15:17:15 +0100

    Update protocol names in database

--- priv/repo/migrations/20250119143802_rename_stream_parser_to_protocol.exs
+++ priv/repo/migrations/20250119143802_rename_stream_parser_to_protocol.exs
@@ -3,5 +3,8 @@ defmodule Asciinema.Repo.Migrations.RenameStreamParserToProtocol do
 
   def change do
     rename table(:live_streams), :parser, to: :protocol
+
+    execute "UPDATE live_streams SET protocol='v0.alis' WHERE protocol='alis'"
+    execute "UPDATE live_streams SET protocol='v2.asciicast' WHERE protocol='json'"
   end
 end
