7ab74ae499eb8c6e40e874828c36fc3739040cf0
Marcin Kulik <m@ku1ik.com>
2025-07-01 10:43:30 +0200

    Disable unused tailwind plugins

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,26 +1,14 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
-const plugin = require("tailwindcss/plugin")
-
 module.exports = {
   content: [
     "./js/**/*.js",
     "../lib/asciinema_web.ex",
     "../lib/asciinema_web/**/*.*ex"
-  ],
+  ]
   theme: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    // Allows prefixing tailwind classes with LiveView classes to add rules
-    // only when LiveView classes are applied, for example:
-    //
-    //     <div class="phx-click-loading:animate-ping">
-    //
-    plugin(({addVariant}) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"]))
-  ]
+  plugins: []
 }
