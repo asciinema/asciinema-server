f27620baa0e0ee87a167e78924516bc36cc075ca
Marcin Kulik <m@ku1ik.com>
2025-07-24 09:31:48 +0200

    Graceful handling of upload-too-big error

--- lib/asciinema_web/controllers/error_json.ex
+++ lib/asciinema_web/controllers/error_json.ex
@@ -10,4 +10,27 @@ defmodule AsciinemaWeb.ErrorJSON do
   def render("404.json", _assigns) do
     %{type: "not_found", message: "Resource not found"}
   end
+
+  def render("413.json", _assigns) do
+    {_, limit, _, _} = AsciinemaWeb.Plug.Parsers.MULTIPART.init([])
+    limit = format_byte_size(limit)
+
+    %{
+      type: "content_too_large",
+      message: "The recording exceeds the server-configured size limit (#{limit})"
+    }
+  end
+
+  defp format_byte_size(size) do
+    cond do
+      rem(size, 1024 * 1024) == 0 ->
+        "#{div(size, 1024 * 1024)} MiB"
+
+      rem(size, 1000 * 1000) == 0 ->
+        "#{div(size, 1000 * 1000)} MB"
+
+      true ->
+        to_string(size)
+    end
+  end
 end
