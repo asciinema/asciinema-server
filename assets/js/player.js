3708ec952867291d46f913cfa726ae06c7748c94
Marcin Kulik <m@ku1ik.com>
2025-12-08 08:45:58 +0100

    Improvements to icons rendering in the player

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,19 +1,48 @@
 import { create } from 'asciinema-player';
 
-export function createPlayer(src, container, opts) {
-  if (opts.customTerminalFontFamily) {
-    opts.terminalFontFamily = `${opts.customTerminalFontFamily},Consolas,Menlo,'Bitstream Vera Sans Mono',monospace,'Powerline Symbols'`;
+const DEFAULT_FONT_FAMILY = 'ui-monospace,"Cascadia Code","Source Code Pro",Menlo,Consolas,"DejaVu Sans Mono",monospace,"Symbols Nerd Font"';
 
-    return document.fonts.load(`1em ${opts.customTerminalFontFamily}`).then(() => {
-      console.log(`loaded font ${opts.customTerminalFontFamily}`);
-      return create(src, container, opts);
-    }).catch(error => {
-      console.log(`failed to load font ${opts.customTerminalFontFamily}`, error);
-      return create(src, container, opts);
-    });
-  } else {
-    return create(src, container, opts);
+export async function createPlayer(src, container, opts) {
+  if (opts.customTerminalFontFamily) {
+    try {
+      if (await isFamilyLoaded(opts.customTerminalFontFamily)) {
+        console.info(`loaded font ${opts.customTerminalFontFamily}`);
+
+        return create(src, container, {
+          ...opts,
+          terminalFontFamily: `'${opts.customTerminalFontFamily}',${DEFAULT_FONT_FAMILY}`
+        });
+      } else {
+        console.error(`font ${opts.customTerminalFontFamily} didn't load`);
+      }
+    } catch (error) {
+      console.error(`failed to load font ${opts.customTerminalFontFamily}:`, error);
+    }
+
+    console.info('falling back to default font family');
   }
+
+  return create(src, container, {
+    ...opts,
+    terminalFontFamily: DEFAULT_FONT_FAMILY
+  });
+}
+
+function normalizedFamily(s) {
+  return s.replace(/^['"]|['"]$/g, "");
+}
+
+async function isFamilyLoaded(family) {
+  const font = `normal 400 normal 1em "${family}"`;
+  const faces = await document.fonts.load(font, "aZ0");
+
+  return faces.some(f =>
+    normalizedFamily(f.family) === family &&
+    String(f.weight) === "400" &&
+    f.style === "normal" &&
+    f.stretch === "normal" &&
+    f.status === "loaded"
+  );
 }
 
 const CONTAINER_VERTICAL_PADDING = 2 * 4;
