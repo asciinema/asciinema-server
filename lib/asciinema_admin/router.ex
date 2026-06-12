9190466495ea90362c2ed2d5e1ed59e546d433e7
Marcin Kulik <m@ku1ik.com>
2026-06-12 16:03:47 +0200

    Use resources macro in admin router

--- lib/asciinema_admin/router.ex
+++ lib/asciinema_admin/router.ex
@@ -24,38 +24,22 @@ defmodule AsciinemaAdmin.Router do
 
     get "/", HomeController, :show
 
-    post "/saved_queries", SavedQueryController, :create
-    put "/saved_queries/:id", SavedQueryController, :update
-    delete "/saved_queries/:id", SavedQueryController, :delete
+    resources "/saved_queries", SavedQueryController, only: [:create, :update, :delete]
 
-    get "/users", UserController, :index
-    get "/users/new", UserController, :new
-    post "/users", UserController, :create
-    get "/users/:id", UserController, :show
-    get "/users/:id/edit", UserController, :edit
-    put "/users/:id", UserController, :update
-    delete "/users/:id", UserController, :delete
+    resources "/users", UserController
     get "/users/:id/merge", UserController, :merge_confirm
     post "/users/:id/merge", UserController, :merge
 
     post "/users/:user_id/clis", CliController, :create
 
-    get "/recordings", RecordingController, :index
-    get "/recordings/:id", RecordingController, :show
+    resources "/recordings", RecordingController, only: [:index, :show, :edit, :update, :delete]
     get "/recordings/:id/file", RecordingController, :cast_file
-    get "/recordings/:id/edit", RecordingController, :edit
-    put "/recordings/:id", RecordingController, :update
-    delete "/recordings/:id", RecordingController, :delete
     post "/recordings/:id/visibility", RecordingController, :set_visibility
     post "/recordings/:id/featured", RecordingController, :set_featured
     post "/recordings/:id/archive_now", RecordingController, :archive_now
     post "/recordings/:id/unarchive", RecordingController, :unarchive
 
-    get "/streams", StreamController, :index
-    get "/streams/:id", StreamController, :show
-    get "/streams/:id/edit", StreamController, :edit
-    put "/streams/:id", StreamController, :update
-    delete "/streams/:id", StreamController, :delete
+    resources "/streams", StreamController, only: [:index, :show, :edit, :update, :delete]
     post "/streams/:id/visibility", StreamController, :set_visibility
     post "/streams/:id/disconnect", StreamController, :disconnect
 
